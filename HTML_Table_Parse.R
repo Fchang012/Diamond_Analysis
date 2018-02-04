2b889c147398f7b0c32e5f5e7cf924d9cd15c257
Frank Chang <fchang7@gatech.edu>
2018-02-03 22:57:40 -0600

    got jamesAllen list in

--- HTML_Table_Parse.R
+++ HTML_Table_Parse.R
@@ -3,12 +3,23 @@ require(RCurl)
 require(rlist)
 require(rvest)
 require(tibble)
+# require(xml2)
+
+# # General Diamond Criteria
+# Shape: Princess
+# Caret: 0.85 - 2.0
+# Color: G - D
+# Clarity: Vs2- FL
+# Depth: 73 - 76
+# Table: 67 - 71
+
 
 # Get cur dir from source of R Script
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
-# Bluenile Table
+
+# Blue Nile ---------------------------------------------------------------
 BlueNileTable <- read_xml("BlueNileTable.html", as_html = T)
 BlueNileTable <- xmlTreeParse(BlueNileTable)[[1]]
 top <- xmlRoot(BlueNileTable)
@@ -27,7 +38,7 @@ eachRecord <- getNodeSet(top, '//a')
 blueNileDataframe <- xmlToDataFrame(nodes = eachRecord)
 urlList <- list()
 for(i in 1:length(eachRecord)){
-  urlList[i] <- xmlGetAttr(eachRecord[[i]], 'href')
+  urlList[i] <- paste('https://www.bluenile.com', substring((xmlGetAttr(eachRecord[[i]], 'href')),2), sep = '')
 }
 urlList <- t(as.data.frame(urlList))
 
@@ -37,3 +48,21 @@ blueNileDataframe <- cbind(blueNileDataframe, urlList)
 rownames(blueNileDataframe) <- NULL
 blueNileDataframe <- as.tibble(blueNileDataframe)
 
+
+# James Allen -------------------------------------------------------------
+JamesAllenTable <- readHTMLTable("JamesAllenWebTable.txt")
+JamesAllenTable <- list.clean(JamesAllenTable, fun = is.null, recursive = FALSE)
+JamesAllenTable <- as.tibble(JamesAllenTable$ResultsTable[,1:11])
+
+# Getting the ids
+JamesAllenIDs <- htmlTreeParse("JamesAllenWebTable.txt")[[1]]
+idList <- list()
+JamesAllenIDs <- getNodeSet(JamesAllenIDs, '//tr[@data-item-id]')
+for(i in 1:length(JamesAllenIDs)){
+  idList[i] <- paste('https://www.jamesallen.com/ese/?q=', xmlAttrs(JamesAllenIDs[[i]])[[5]], sep = '')
+}
+
+idList <- t(as.data.frame(idList))
+rownames(idList) <- NULL
+colnames(idList) <- "James_Allen_id"
+JamesAllenTable <- cbind(JamesAllenTable, idList)
