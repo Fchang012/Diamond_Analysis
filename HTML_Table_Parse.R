caeab32b42901ff8800562f3e7ea7da17f9a20b1
Frank Chang <fchang7@gatech.edu>
2018-02-03 19:35:50 -0600

    finally figured out how to get it into R

--- HTML_Table_Parse.R
+++ HTML_Table_Parse.R
@@ -1,11 +1,39 @@
 require(XML)
 require(RCurl)
 require(rlist)
+require(rvest)
+require(tibble)
 
 # Get cur dir from source of R Script
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
 # Bluenile Table
-tables <- readHTMLTable("SearchforDiamondsbyShape SizeQualityPrice   BlueNile.html")
-str(tables)
+BlueNileTable <- read_xml("BlueNileTable.html", as_html = T)
+BlueNileTable <- xmlTreeParse(BlueNileTable)[[1]]
+top <- xmlRoot(BlueNileTable)
+
+# headers
+headers <- getNodeSet(top, '//div[@class="row"]')
+headers <- getNodeSet(headers[[1]], '//div')
+headers <- headers[-1]
+headerList <- list()
+for(i in 1:length(headers)){
+  headerList[i] <- xmlValue(headers[[i]])
+}
+
+# each records
+eachRecord <- getNodeSet(top, '//a')
+blueNileDataframe <- xmlToDataFrame(nodes = eachRecord)
+urlList <- list()
+for(i in 1:length(eachRecord)){
+  urlList[i] <- xmlGetAttr(eachRecord[[i]], 'href')
+}
+urlList <- t(as.data.frame(urlList))
+
+# Create df
+names(blueNileDataframe) <- headerList
+blueNileDataframe <- cbind(blueNileDataframe, urlList)
+rownames(blueNileDataframe) <- NULL
+blueNileDataframe <- as.tibble(blueNileDataframe)
+
